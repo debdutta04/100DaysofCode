2e54f71dfe784a5ccb62696fc27f7e30c74ec29f
Debdutta Burman <111264897+debdutta04@users.noreply.github.com>
2023-08-20 20:26:55 +0530

    Update secondLargestElement.cpp

--- Day1/secondLargestElement.cpp
+++ Day1/secondLargestElement.cpp
@@ -1,9 +1,32 @@
+#include <bits/stdc++.h> 
 #include <iostream>
 using namespace std;
 
-int main(){
-
-
-    
-    return 0;
+int findSecondLargest(int n, vector<int> &arr)
+{
+  
+    sort(arr.begin(),arr.end());
+    for(int i=n-2; i>=0; i--){
+        if(arr[i] != arr[n-1])
+            return arr[i];
+        }
+    return -1;
 }
+
+    int main(){
+	
+	int n;
+    cout<<"Enter a size of Array";
+	cin>>n;
+	vector<int> arr;
+	for(int i=0; i<n; i++){
+		int element =0;
+		cin>>element;
+		arr.push_back(element);
+	}
+	
+	int ans = findSecondLargest(n, arr);
+	cout<<"Second largest element is "<<ans;
+	return 0;
+}
+
