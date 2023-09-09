b5243ba5f8c6ce90e078cff648a282d8d6d004d6
Debdutta Burman <111264897+debdutta04@users.noreply.github.com>
2023-09-09 20:42:32 +0530

    Update beautifulString.cpp

--- Day4/beautifulString.cpp
+++ Day4/beautifulString.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
- return 0;
+int makeBeautiful(string str) {
+	int ans =0;
+	int len = str.length();
+	
+	for(int i=0; i< len; i++){
+
+		
+	//If there is 1 at Even index positions
+	if(i%2 == 0 && str[i] == '1')
+		ans++;
+	
+	//If there is 0 at odd index positions
+	if(i%2 != 0 && str[i]=='0')
+		ans++;
+	
+}
+	return min(ans, len-ans);
 }
